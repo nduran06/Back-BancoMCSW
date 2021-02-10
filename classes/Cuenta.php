b2c1aa4f298dcb4146c55b347112510161c415d0
jerv0929 <78751551+jerv0929@users.noreply.github.com>
2021-02-10 13:08:25 -0500

    Update Cuenta.php

--- classes/Cuenta.php
+++ classes/Cuenta.php
@@ -5,12 +5,16 @@
         private $_balance;
         private $_type;
         private $_state;
+        private $_userID;
+        private $_bancoID;
 
-        function __construct($_number, $_balance, $_type, $_state) {
+        function __construct($_number, $_balance, $_type, $_state, $_userID, $_bancoID ) {
             $this->_number = $_number;
             $this->_balance = $_balance;
             $this->_type = $_type;
             $this->_state = $_state;
+            $this->_state = $_userID;
+            $this->_state = $_bancoID;
         }
 
         public function getNumber(){
@@ -25,6 +29,12 @@
         public function getState(){
             return $this->_state;
         }
+        public function getUser(){
+            return $this->_userID;
+        }
+        public function getBanco(){
+            return $this->_bancoID;
+        }
 
         public function setNumber($number){
             $this->_number=$number;
@@ -38,6 +48,12 @@
         public function setState($state){
             $this->_state=$state;
         }
-
+        public function setType($userID){
+            $this->_type=$userID;
+        }
+        public function setState($bancoID){
+            $this->_state=$bancoID;
+        }
+        
     }
 
