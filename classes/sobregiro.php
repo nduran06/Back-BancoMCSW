7e7b8fd6d9208a0b6e8133f124d7c5daf375ab38
jerv0929 <78751551+jerv0929@users.noreply.github.com>
2021-02-11 17:07:57 -0500

    Update sobregiro.php

--- classes/sobregiro.php
+++ classes/sobregiro.php
@@ -4,13 +4,13 @@ class Sobregiro {
         private $_cuentaExistID;
         private $_state;
         private $_percent;
-       
+
 
         function __construct($_cuentaID, $_state, $_percent ) {
-            $this->_cuentaExistID = $_cuentaExistID;
+            $this->_cuentaExistID = $_cuentaID;
             $this->_state = $_state;
             $this->_percent = $_percent;
-           
+
         }
 
         public function getCuentaExistID(){
@@ -22,7 +22,7 @@ class Sobregiro {
         public function getPercent(){
             return $this->_percent;
         }
-        
+
         public function setCuentaExistID($cuentaExistID){
             $this->_number=$cuentaExistID;
         }
@@ -32,6 +32,6 @@ class Sobregiro {
         public function setPercent($percent){
             $this->_type=$percent;
         }
-        
-        
+
+
     }
